5088ca6376e42ca634713476857115a77113e4f5
rob <Duckz@protonmail.ch>
2023-01-22 03:04:09 -0700

    updated

--- shell/shortcutrc/shortcutrc.plugin.zsh
+++ shell/shortcutrc/shortcutrc.plugin.zsh
@@ -1,28 +1,28 @@
 alias \
 	h="cd $HOME/ && ls -a" \
 	c="calcurse" \
-	d="cd $HOME/doc && lf" \
+	d="cd $HOME/doc && cf" \
 	n="$EDITOR $MGH/notes/index.md" \
-	D="cd $HOME/dl && lf" \
-	mu="cd $HOME/doc/media/mu && lf" \
-	img="cd $HOME/doc/media/img && lf" \
-	vid="cd $HOME/doc/media/vid && lf" \
-	gh="cd $GH && lf" \
-	mgh="cd $MGH && lf" \
-	gp="cd $GOPATH && lf" \
-	lc="cd $HOME/.local && lf" \
+	D="cd $HOME/dl && cf" \
+	mu="cd $HOME/doc/media/mu && cf" \
+	img="cd $HOME/doc/media/img && cf" \
+	vid="cd $HOME/doc/media/vid && cf" \
+	gh="cd $GH && cf" \
+	mgh="cd $MGH && cf" \
+	gp="cd $GOPATH && cf" \
+	lc="cd $HOME/.local && cf" \
 	sc="cd $HOME/.local/bin && ls -a" \
 	mn="cd /mnt && ls -a" \
 	bf="$EDITOR $XDG_CONFIG_HOME/files" \
 	bd="$EDITOR $XDG_CONFIG_HOME/directories" \
 	bw="$EDITOR $XDG_CONFIG_HOME/bookmarks" \
-	cfa="$EDITOR $XDG_CONFIG_HOME/shell/aliasrc" \
-	cfs="$EDITOR $XDG_CONFIG_HOME/shell/shortcutrc" \
+	cfa="$EDITOR $XDG_CONFIG_HOME/zsh/plugins/zsh-config-as-plugin/shell/aliasrc/aliasrc.plugin.zsh" \
+	cfs="$EDITOR $XDG_CONFIG_HOME/zsh/plugins/zsh-config-as-plugin/shell/shortcutrc/shortcutrc.plugin.zsh" \
 	cfkb="cd /home/dwm/qmk_firmware/keyboards/preonic/keymaps/duckzcantfly && $EDITOR keymap.c" \
 	cfz="$EDITOR $ZDOTDIR/.zshrc" \
-	cfzp="lf $ZPLUGINDIR/zsh-config-as-plugin" \
+	cfzp="cf $ZPLUGINDIR/zsh-config-as-plugin" \
 	cfc="$EDITOR $XDG_DATA_HOME/calcurse/apts" \
-	cfv="$EDITOR $XDG_CONFIG_HOME/nvim/init.vim" \
+	cfv="$EDITOR $XDG_CONFIG_HOME/nvim" \
 	cfm="$EDITOR $XDG_CONFIG_HOME/mutt/muttrc" \
 	cfd="$EDITOR $HOME/.Xdefaults" \
 	cfu="$EDITOR $XDG_CONFIG_HOME/newsboat/urls" \
@@ -32,7 +32,7 @@ alias \
 	cfdc="dconf dump / >$XDG_CONFIG_HOME/dconf/user.conf \
 		&& $EDITOR -c 'setf dosini' $XDG_CONFIG_HOME/dconf/user.conf \
 		&& dconf load / <$XDG_CONFIG_HOME/dconf/user.conf" \
-	s="cd $HOME/doc/school && lf" \
-	ltn="cd $HOME/doc/media/books/ln && lf" \
-	b="cd $HOME/doc/books && lf" \
+	s="cd $HOME/doc/school && cf" \
+	ltn="cd $HOME/doc/media/books/ln && cf" \
+	b="cd $HOME/doc/books && cf" \
 	gop="cd $XDG_DATA_HOME/go" \
