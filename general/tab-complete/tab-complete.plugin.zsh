0882cb73d84afaa799c088e94dad04b442bd7102
rob <Duckz@protonmail.ch>
2023-01-21 21:59:44 -0700

    fixed tab completion

--- general/tab-complete/tab-complete.plugin.zsh
+++ general/tab-complete/tab-complete.plugin.zsh
@@ -1,11 +1,12 @@
 ### Basic Tab Complete ###
 
 fpath=(${ZDOTDIR}/completion $fpath)
-autoload -U compinit
+autoload -U compinit ; compinit
+setopt MENU_COMPLETE
+zmodload zsh/complist
+eval "$(dircolors)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zmodload zsh/complist
-compinit
 #_comp_options+=(globdots)		# Include hidden files.
 # local completions
-setopt MENU_COMPLETE
