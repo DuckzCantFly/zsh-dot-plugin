ac26b507c35dce349d8190b5cd580dd0055a556e
rob <Duckz@protonmail.ch>
2023-01-21 22:12:22 -0700

    tweaks

--- general/vi-mode/vi-mode.plugin.zsh
+++ general/vi-mode/vi-mode.plugin.zsh
@@ -9,25 +9,24 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
 
-## Change cursor shape for different vi modes.
-function _zle-keymap-select () {
+# Change cursor shape for different vi modes.
+function zle-keymap-select() {
 		case $KEYMAP in
 				vicmd) echo -ne '\e[1 q';;      # block
 				viins|main) echo -ne '\e[5 q';; # beam
 		esac
-}
-zle -N _zle-keymap-select
+} ; zle -N zle-keymap-select
 
-
-zle-line-init() {
+function zle-line-init() {
 		# initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-		zle -K viins
+		zle -K viins #
 		echo -ne "\e[5 q"
-}
-zle -N zle-line-init
+} ; zle -N zle-line-init
 
 # Use beam shape cursor on startup.
 echo -ne '\e[5 q'
+
 # Use beam shape cursor for each new prompt.
 preexec() { echo -ne '\e[5 q' ;}
