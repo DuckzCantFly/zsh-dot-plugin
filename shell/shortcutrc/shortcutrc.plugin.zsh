7c009a6d8680b238c176a3cc2ee03634ea492f70
rob <Duckz@protonmail.ch>
2023-01-23 01:49:00 -0700

    better lf

--- shell/shortcutrc/shortcutrc.plugin.zsh
+++ shell/shortcutrc/shortcutrc.plugin.zsh
@@ -1,17 +1,19 @@
+### shortcutrc ###
+
 alias \
 	h="cd $HOME/ && ls -a" \
 	c="calcurse" \
-	d="cd $HOME/doc && cf" \
+	d="cf $HOME/doc" \
 	n="$EDITOR $MGH/notes/index.md" \
-	D="cd $HOME/dl && cf" \
-	mu="cd $HOME/doc/media/mu && cf" \
-	img="cd $HOME/doc/media/img && cf" \
-	vid="cd $HOME/doc/media/vid && cf" \
-	gh="cd $GH && cf" \
-	mgh="cd $MGH && cf" \
-	gp="cd $GOPATH && cf" \
-	lc="cd $HOME/.local && cf" \
-	sc="cd $HOME/.local/bin && ls -a" \
+	D="cf $HOME/dl" \
+	mu="cf $HOME/doc/media/mu" \
+	img="cf $HOME/doc/media/img" \
+	vid="cf $HOME/doc/media/vid" \
+	gh="cf $GH" \
+	mgh="cf $MGH" \
+	gp="cf $GOPATH" \
+	lc="cf $HOME/.local" \
+	sc="cf $HOME/.local/bin-a" \
 	mn="cd /mnt && ls -a" \
 	bf="$EDITOR $XDG_CONFIG_HOME/files" \
 	bd="$EDITOR $XDG_CONFIG_HOME/directories" \
@@ -19,8 +21,7 @@ alias \
 	cfa="$EDITOR $XDG_CONFIG_HOME/zsh/plugins/zsh-config-as-plugin/shell/aliasrc/aliasrc.plugin.zsh" \
 	cfs="$EDITOR $XDG_CONFIG_HOME/zsh/plugins/zsh-config-as-plugin/shell/shortcutrc/shortcutrc.plugin.zsh" \
 	cfkb="cd /home/dwm/qmk_firmware/keyboards/preonic/keymaps/duckzcantfly && $EDITOR keymap.c" \
-	cfz="$EDITOR $ZDOTDIR/.zshrc" \
-	cfzp="cf $ZPLUGINDIR/zsh-config-as-plugin" \
+	cfzp="$EDITOR $ZPLUGINDIR/zsh-config-as-plugin" \
 	cfc="$EDITOR $XDG_DATA_HOME/calcurse/apts" \
 	cfv="$EDITOR $XDG_CONFIG_HOME/nvim" \
 	cfm="$EDITOR $XDG_CONFIG_HOME/mutt/muttrc" \
@@ -32,7 +33,7 @@ alias \
 	cfdc="dconf dump / >$XDG_CONFIG_HOME/dconf/user.conf \
 		&& $EDITOR -c 'setf dosini' $XDG_CONFIG_HOME/dconf/user.conf \
 		&& dconf load / <$XDG_CONFIG_HOME/dconf/user.conf" \
-	s="cd $HOME/doc/school && cf" \
-	ltn="cd $HOME/doc/media/books/ln && cf" \
-	b="cd $HOME/doc/books && cf" \
+	s="cf $HOME/doc/school" \
+	ltn="cf $HOME/doc/media/books/ln" \
+	b="cf $HOME/doc/books" \
 	gop="cd $XDG_DATA_HOME/go" \
