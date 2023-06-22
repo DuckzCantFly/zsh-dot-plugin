97f4f343b2df46cff05b72aebcb587c8bc986ac4
rob <Duckz@protonmail.ch>
2023-06-22 13:20:00 -0600

    updated distro id dectiction

--- general/general.plugin.zsh
+++ general/general.plugin.zsh
@@ -2,14 +2,26 @@
 autoload -U colors && colors	2>/dev/null # Load colors
 
 # Grab Distro ID
-if lsb_release 1> /dev/null 2>&1 ; then
-	export SYS_DISTRO_ID="$(lsb_release -a | grep -e "Distributor ID:" | cut -d ':' -f2 | tr -d '[:space:]')"
-elif ls -U /etc/*release 1> /dev/null 2>&1 ; then
-	export SYS_DISTRO_ID="$(cat /etc/*release | grep DISTRIB_ID | cut -d '=' -f2 | tr -d '[:space:]')"
-elif ls -U /etc/issue* 1> /dev/null 2>&1 ; then
-	export SYS_DISTRO_ID="$(cat /etc/issue* | cut -d ' ' -f1 | tr -d '[:space:]')"
+
+if [ -n "${SYS_DISTRO_ID}" ] ; then
+elif [ -x "$(command -v lsb_release)" ] ; then
+	SYS_DISTRO_ID="$(lsb_release -a | grep -e "Distributor ID:" | cut -d ':' -f2 | tr -d '[:space:]')"
+elif 
+	if ls -U /etc/*release 1>& /dev/null ;then
+		SYS_DISTRO_ID="$(cat /etc/*release | grep DISTRIB_ID | cut -d '=' -f2 | tr -d '[:space:]')"
+	else 
+		[ 1 = 2 ]
+	fi ; [ -n "${SYS_DISTRO_ID}" ]
+then
+elif 
+	if ls -U /etc/issue* 1>& /dev/null ; then
+		SYS_DISTRO_ID="$(cat /etc/issue* | cut -d ' ' -f1 | tr -d '[:space:]')"
+	else
+		[ 1 = 2 ]
+	fi ; [ -n "${SYS_DISTRO_ID}" ]
+then
 else
-	export SYS_DISTRO_ID="other"
+	SYS_DISTRO_ID="other"
 fi
 
 ## Set ZDOTDIR
