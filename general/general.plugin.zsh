723f10df77b2019ff61020f4434dfd53874f02e7
DuckzCantFly <Duckz@protonmail.ch>
2023-10-11 13:05:58 -0600

    break up long distro set lines

--- general/general.plugin.zsh
+++ general/general.plugin.zsh
@@ -3,18 +3,32 @@ autoload -U colors && colors	2>/dev/null # Load colors
 
 # Grab Distro ID
 if [ -x "$(command -v lsb_release)" ] ; then
-	LOCAL_SYS_DISTRO_ID="$(lsb_release -a | grep -e "Distributor ID " | cut -d ':' -f2 | tr -d '[:space:]')"
+	LOCAL_SYS_DISTRO_ID="$(
+		lsb_release -a \
+		| grep -i -e "Distributor ID:" \
+		| cut -d ':' -f2 \
+		| tr -d '[:space:]'
+	)"
 	return
 elif 
 	if ls -U /etc/*release 1>& /dev/null 2>&1 ;then
-		LOCAL_SYS_DISTRO_ID="$(cat /etc/*release | grep -m 1 "ID=" | cut -d '=' -f2 | tr -d '[:space:]')"
+		LOCAL_SYS_DISTRO_ID="$(
+			cat /etc/*release \
+			| grep -m 1 "ID=" \
+			| cut -d '=' -f2 \
+			| tr -d '[:space:]'
+		)"
 	else 
 		[ 1 = 2 ]
 	fi ; [ -n "${LOCAL_SYS_DISTRO_ID}" ]
 then ; return
 elif 
 	if ls -U /etc/issue* 1>& /dev/null ; then
-		LOCAL_SYS_DISTRO_ID="$(cat /etc/issue* | cut -d ' ' -f1 | tr -d '[:space:]')"
+		LOCAL_SYS_DISTRO_ID="$(
+			cat /etc/issue* \
+			| cut -d ' ' -f1 \
+			| tr -d '[:space:]'
+		)"
 	else
 		[ 1 = 2 ]
 	fi ; [ -n "${LOCAL_SYS_DISTRO_ID}" ]
