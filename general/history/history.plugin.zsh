9ad399375d239689d06f193997cbfb13d9547dd5
DuckzCantFly <Duckz@protonmail.ch>
2023-10-11 12:44:47 -0600

    Improve history defaults

--- general/history/history.plugin.zsh
+++ general/history/history.plugin.zsh
@@ -20,12 +20,12 @@ setopt HIST_EXPIRE_DUPS_FIRST # Expire duplicate entries first when trimming his
 setopt HIST_FIND_NO_DUPS      # Do not display a line previously found.
 setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits.
 
-# setopt EXTENDED_HISTORY       # Write the history file in the ":start:elapsed;command" format.
+setopt EXTENDED_HISTORY       # Write the history file in the ":start:elapsed;command" format.
+setopt HIST_REDUCE_BLANKS     # Remove superfluous blanks before recording entry.
 # setopt HIST_BEEP              # Beep when accessing nonexistent history.
 # setopt HIST_IGNORE_DUPS       # Don't record an entry that was just recorded again.
 # setopt HIST_IGNORE_ALL_DUPS   # Delete old recorded entry if new entry is a duplicate.
-# setopt HIST_IGNORE_SPACE      # Don't record an entry starting with a space.
-# setopt HIST_REDUCE_BLANKS     # Remove superfluous blanks before recording entry.
 # setopt HIST_SAVE_NO_DUPS      # Don't write duplicate entries in the history file.
+# setopt HIST_IGNORE_SPACE      # Don't record an entry starting with a space.
 # setopt HIST_VERIFY            # Don't execute immediately upon history expansion.
 # setopt SHARE_HISTORY          # Share history between all sessions.
