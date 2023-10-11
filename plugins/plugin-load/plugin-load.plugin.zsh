22663ae0d106747eb03a6856ffb838c776efe1d3
DuckzCantFly <Duckz@protonmail.ch>
2023-10-11 12:30:36 -0600

    load faster alais tip

--- plugins/plugin-load/plugin-load.plugin.zsh
+++ plugins/plugin-load/plugin-load.plugin.zsh
@@ -3,14 +3,17 @@
 # Plugins to Load
 plugins=(
 	zsh-defer # Only put plugins after this one if not needed imedatly at start.
+) ; plugin-source ${plugins}
+
+plugins=(
 	zsh-completions
 	zsh-history-substring-search
 	zsh-autosuggestions
 	fast-syntax-highlighting
 	zsh-autopair
-	alias-tips
 	zsh-system-clipboard
-) ; plugin-source ${plugins}
+	zsh-you-should-use 
+) ; zsh-defer plugin-source ${plugins}
 
 # Load Zsh Bench
-# export PATH="${ZPLUGINDIR}/zsh-bench:$PATH"
+export PATH="${ZPLUGINDIR}/zsh-bench:$PATH"
